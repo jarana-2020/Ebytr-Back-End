0959381f0cd3f684196b3b2cd8cfa03aa57f04a2
Julio <julioaranabr@yahoo.com.br>
2022-05-18 17:38:48 -0300

    created routes

--- src/api/Routes/todos.routes.ts
+++ src/api/Routes/todos.routes.ts
@@ -1,22 +1,24 @@
 import { Router } from 'express';
+import { todosController } from '../../app/controller/todos.controller';
 
 
 const todosRoute = Router();
 
 todosRoute.put('/:id', async (req, res) => {
-  
+  res.send(req);
 });
 
 todosRoute.delete('/:id', async (req, res) => {
-  
+  res.send(req);
 });
 
-todosRoute.get('/', async (req, res) => {
-  
+todosRoute.get('/', async (_req, res) => {
+  const result = await todosController.list();
+  res.json(result);
 });
 
 todosRoute.post('/', async (req, res) => {
-  
+  res.send(req);
 });
 
 export default todosRoute;
