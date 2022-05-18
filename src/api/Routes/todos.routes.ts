74e2addee65c30e759d510a09c0aa32baada4b85
Julio <julioaranabr@yahoo.com.br>
2022-05-18 12:27:10 -0300

    configure routes

---
+++ src/api/Routes/todos.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+
+const todosRoute = Router();
+
+todosRoute.put('/:id', async (req, res) => {
+  
+});
+
+todosRoute.delete('/:id', async (req, res) => {
+  
+});
+
+todosRoute.get('/', async (req, res) => {
+  
+});
+
+todosRoute.post('/', async (req, res) => {
+  
+});
+
+export default todosRoute;
