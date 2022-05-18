7048204337a98fe291a127988258ae02bb492dcb
Julio <julioaranabr@yahoo.com.br>
2022-05-18 17:15:47 -0300

    created dbconnet

---
+++ src/db/index.ts
@@ -0,0 +1,11 @@
+import { connect } from 'mongoose';
+import utils from '../utils';
+import makeTodosDAO from './todos.dao';
+
+export const todoDAO = makeTodosDAO();
+
+export const dbConnect = {
+  async authenticate(): Promise<void> {
+    await connect(utils.db.uri);
+  }
+};
