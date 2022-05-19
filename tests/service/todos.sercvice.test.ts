83cdc0b2cc2051f966eb95d8f78277f86d46586b
Julio <julioaranabr@yahoo.com.br>
2022-05-18 21:20:50 -0300

    test service method list

---
+++ tests/service/todos.sercvice.test.ts
@@ -0,0 +1,23 @@
+import {expect, use} from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import sinon from 'sinon';
+import { todosModel } from '../../src/app/model/todos.model';
+import { todoService } from '../../src/app/service/todo.service';
+
+use(chaiAsPromised);
+
+describe('Test Service todos.service', () => {
+  beforeEach(() => {
+    sinon.restore();
+  });
+
+  it('should throw error ',() => {
+    sinon.stub(todosModel, 'list').rejects();
+    expect(todoService.list()).to.eventually.to.be.rejected;
+  });
+
+  it('should return a array', () => {
+    sinon.stub(todosModel, 'list').resolves([]);
+    expect(todoService.list()).to.eventually.to.deep.equal([]);
+  });
+});
