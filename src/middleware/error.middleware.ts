7504af1d39c27d44eb6ac829bdc3079435854b47
Julio <julioaranabr@yahoo.com.br>
2022-05-19 16:22:53 -0300

    adjust errorHandler

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -6,8 +6,14 @@ export const errorHandlerMiddleware = (
   _req:Request, 
   res: Response, 
   _next: NextFunction): void  => {
-  const { name } = err;
+  const { name, message } = err;
   switch(name) {
+  case 'ValidationError':
+    res.status(400).json({ message});
+    break;
+  case 'NotFoundError':
+    res.status(400).json({ message });
+    break;
   default:
     res.status(500);
   }
