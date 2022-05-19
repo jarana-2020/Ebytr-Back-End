5f470015ba22819f00eb20118a48600ed6111935
Julio <julioaranabr@yahoo.com.br>
2022-05-18 22:58:03 -0300

    added method add in controller

--- src/app/controller/todos.controller.ts
+++ src/app/controller/todos.controller.ts
@@ -1,9 +1,17 @@
 import { Todo } from '../../Types/todosTypes';
-import { todoService } from '../service/todo.service';
+import { todoService } from '../service/todos.service';
+import { todosValidator } from './validators/todos.validator';
 
 export const todosController = {
   async list(): Promise<Todo[]> {
     const result = await todoService.list();
+    return result;
+  },
+
+  async add(body: unknown): Promise<Todo> {
+    const data = await todosValidator.bodyAdd(body);
+    const id = await todoService.add(data);
+    const result = await todoService.get(id);
     return result;
   }
 };
