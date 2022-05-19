2abc9b9bbd7cde588d2a413187db7ac02d3d866b
Julio <julioaranabr@yahoo.com.br>
2022-05-19 12:13:38 -0300

    test methods add todos.controller

--- tests/controller/todos.controller.test.ts
+++ tests/controller/todos.controller.test.ts
@@ -2,22 +2,59 @@ import {expect, use} from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 import { todosController } from '../../src/app/controller/todos.controller';
+import { todosValidator } from '../../src/app/controller/validators/todos.validator';
 import { todoService } from '../../src/app/service/todos.service';
+import { Todo } from '../../src/Types/todosTypes';
 
 use(chaiAsPromised);
 
 describe('Test Model todos.model', () => {
-  beforeEach(() => {
-    sinon.restore();
-  });
+
+  describe('Test method list', () => {
+    beforeEach(() => {
+      sinon.restore();
+    });
+    
+    it('should return throw', () => {
+      sinon.stub(todoService, 'list').rejects();
+      expect(todosController.list()).to.eventually.to.be.rejected;
+    });
   
-  it('should return throw', () => {
-    sinon.stub(todoService, 'list').rejects();
-    expect(todosController.list()).to.eventually.to.be.rejected;
+    it('should return a array', () => {
+      sinon.stub(todoService, 'list').resolves([]);
+      expect(todosController.list()).to.eventually.to.deep.equal([]);
+    });
   });
 
-  it('should return a array', () => {
-    sinon.stub(todoService, 'list').resolves([]);
-    expect(todosController.list()).to.eventually.to.deep.equal([]);
+  describe('Test method add', () => {
+    beforeEach(() => {
+      sinon.restore();
+    });
+    
+    it('todosValidator should return throw', () => {
+      sinon.stub(todosValidator, 'bodyAdd').rejects();
+      expect(todosController.add({} as any)).to.eventually.to.be.rejected;
+    });
+
+    it('todoService.add should return throw', () => {
+      sinon.stub(todosValidator, 'bodyAdd').resolves({} as any);
+      sinon.stub(todoService, 'add').rejects({} as any);
+      expect(todosController.add({} as any)).to.eventually.to.be.rejected;
+    });
+
+    it('todoService.get should return throw', () => {
+      sinon.stub(todosValidator, 'bodyAdd').resolves({} as any);
+      sinon.stub(todoService, 'add').resolves('1');
+      sinon.stub(todoService, 'get').rejects({} as any);
+      expect(todosController.add({} as any)).to.eventually.to.be.rejected;
+    });
+
+  
+    it('should return a object', () => {
+      sinon.stub(todosValidator, 'bodyAdd').resolves({} as any);
+      sinon.stub(todoService, 'add').resolves('1');
+      sinon.stub(todoService, 'get').resolves({} as Todo);
+      expect(todosController.list()).to.eventually.to.deep.equal([]);
+    });
   });
 });
